812a4b9f5b5de34bdf3b85aebbb2f8e83d802fd5
aniket salunke <aniket3984@gmail.com>
2024-04-24 02:27:06 +0530

    setting up react image gallery

--- src/Components/Screens/Product-details/ProductDetailsLatest.js
+++ src/Components/Screens/Product-details/ProductDetailsLatest.js
@@ -1,28 +1,65 @@
-import React, { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+import { Box, Grid } from "@mui/material";
 
 export const ProductDetailsLatest = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const [productDetails, setProductDetails] = useState({
-        product_name: '',
-        metal_description: '',
-        metal_amount: '',
-        item_description: '',
-        purity: '',
-        description: '',
-        sales_price: 0,
-        gross_wt: 0
-    })
+  const [productDetails, setProductDetails] = useState({
+    product_name: "",
+    metal_description: "",
+    metal_amount: "",
+    item_description: "",
+    purity: "",
+    description: "",
+    sales_price: 0,
+    gross_wt: 0,
+  });
 
-    return (
+  //   images array for image gallery
 
-        <div className='container'>
-            <div className="">
-                {/* product images gallery */}
-                
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages([
+      {
+        url: "https://source.unsplash.com/random/?sig=1&count=5",
+        thumbnailUrl: "https://source.unsplash.com/random/?sig=1&count=5",
+      },
+      {
+        url: "https://source.unsplash.com/random/?sig=1&count=5",
+        thumbnailUrl: "https://source.unsplash.com/random/?sig=1&count=5",
+      },
+    ]);
+  }, []);
+
+  const imageItems = images.map((image) => ({
+    original: image.url,
+    thumbnail: image.thumbnailUrl,
+  }));
+
+  return (
+    <Box>
+      <Grid container spacing={1} className="p-3">
+        <Grid item md={6}>
+          <div className="">
+            {/* product images gallery */}
+            <div className="product-gallery">
+              <ImageGallery items={imageItems} showNav={false} showPlayButton={false}  />
             </div>
-        </div>
-    )
-}
+          </div>
+        </Grid>
+
+        <Grid item md={6}>
+          <div className="">
+            {/* section for product information */}
+            <h1>This is product information</h1>
+            </div>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};

80f250403a5e0eb99b3a6b8cd4cff13e72f49fe2
aniket salunke <aniket3984@gmail.com>
2024-04-27 18:09:06 +0530

    changes done for product detail, added enquiry dialog

---
+++ src/Components/Screens/Product-details/EnquiryModal.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Modal,
+  Fade,
+  Button,
+  Typography,
+  TextField,
+  IconButton,
+  DialogActions,
+  Grid,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import EastIcon from "@mui/icons-material/East";
+import { useNavigate } from "react-router-dom";
+import successCheckIcon from "../../../images/icons/successful-checkbox.svg";
+import productImage from "../../../images/MaskGroup18.png";
+
+const Boxstyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const EnquiryModal = ({ open, handleClose }) => {
+  const [showEnquiryScreen, setShowEnquiryScreen] = useState(true);
+  const [showThankYouScreen, setShowThankYouScreen] = useState(false);
+
+  const handleCloseButton = () => {
+    setShowThankYouScreen(false);
+    setShowEnquiryScreen(true);
+    handleClose();
+  };
+
+  const handleGetUpdates = () => {
+    setShowEnquiryScreen(false);
+    setShowThankYouScreen(true);
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        className="enquiry-modal"
+      >
+        <Fade in={open}>
+          <Box
+            sx={Boxstyle}
+            style={{ border: "12px solid #ede5e5", width: "50em" }}
+          >
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <div style={{ flex: "1" }}>
+                <img
+                  src={productImage}
+                  className="side-product-image"
+                  alt=""
+                />
+              </div>
+
+              <div style={{ flex: "1" }}>
+                <DialogActions>
+                  <IconButton aria-label="close" onClick={handleCloseButton}>
+                    <CloseIcon />
+                  </IconButton>
+                </DialogActions>
+                {showEnquiryScreen && (
+                  <div className="container enquiry-form">
+                    <Typography
+                      variant="h3"
+                      className="form-title"
+                      gutterBottom
+                    >
+                      Thank you for inquiring with us.
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      className="form-subtitle"
+                      gutterBottom
+                    >
+                      Soon we will reach out to you!!
+                    </Typography>
+                    <TextField
+                      label="Name"
+                      className="mb-2"
+                      fullWidth
+                      required
+                    />
+                    <TextField
+                      label="Phone Number"
+                      className="mb-4"
+                      fullWidth
+                      required
+                    />
+                    <Button
+                      className="btn btn-block bg-black btn-submit col-12 col-md-10 col-lg-6"
+                      variant="contained"
+                      fullWidth
+                      onClick={handleGetUpdates}
+                    >
+                      <span>GET UPDATES</span>
+                      &nbsp;&nbsp;
+                      <EastIcon />
+                    </Button>
+                  </div>
+                )}
+                {showThankYouScreen && (
+                  <div className="container text-center">
+                    <img
+                      src={successCheckIcon}
+                      style={{ width: "123px", height: "123px" }}
+                      alt="Tick Mark"
+                    />
+                    <Typography
+                      variant="h3"
+                      className="form-title"
+                      gutterBottom
+                    >
+                      Inquiry submitted successfully
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      className="form-subtitle"
+                      gutterBottom
+                    >
+                      Our team member will contact you in 24-48 hours
+                    </Typography>
+                  </div>
+                )}
+              </div>
+            </div>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default EnquiryModal;
--- src/Components/Screens/Product-details/ProductDetailsLatest.js
+++ src/Components/Screens/Product-details/ProductDetailsLatest.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
+import EastIcon from "@mui/icons-material/East";
+import EnquiryModal from "./EnquiryModal";
 
 export const ProductDetailsLatest = () => {
   const { id } = useParams();
@@ -46,8 +48,12 @@ export const ProductDetailsLatest = () => {
         thumbnailUrl: "https://source.unsplash.com/random/?sig=1&count=5",
       },
       {
-        url: "https://source.unsplash.com/random/?sig=1&count=5",
-        thumbnailUrl: "https://source.unsplash.com/random/?sig=1&count=5",
+        url: "https://source.unsplash.com/random/?sig=2&count=5",
+        thumbnailUrl: "https://source.unsplash.com/random/?sig=2&count=5",
+      },
+      {
+        url: "https://source.unsplash.com/random/?sig=3&count=5",
+        thumbnailUrl: "https://source.unsplash.com/random/?sig=3&count=5",
       },
     ]);
   }, []);
@@ -56,6 +62,16 @@ export const ProductDetailsLatest = () => {
     original: image.url,
     thumbnail: image.thumbnailUrl,
   }));
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpenEnquiryModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseEnquiryModal = () => {
+    setOpen(false);
+  };
 
   return (
     <Box className="product-details-latest">
@@ -75,13 +91,75 @@ export const ProductDetailsLatest = () => {
 
         <Grid item md={6}>
           <div className="">
-            {/* section for product information */}
-            <h1>This is product information</h1>
-
             <span className="new-arrival-badge">NEW ARRIVAL</span>
 
             <h2 className="product-name">Ganesha Diamond Ring</h2>
+
+            <div className="metal-description">24 KT | 1 GM</div>
+
+            <div className="product-price"> &#8377; 94000</div>
+
+            <div className="product-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+
+            <div className="product-informations my-2">
+              <span className="product-details-title-link">
+                PRODUCT DETAILS
+              </span>
+
+              <div>Gross weight: 64g</div>
+              <div>Net weight: 64g</div>
+              <div>Purity: </div>
+              <div>Stone1 weight: </div>
+              <div>Stone2 weight: </div>
+            </div>
           </div>
+
+          <table border={2} className="product-information-table my-2">
+            <tr>
+              <td>Rate:</td>
+              <td> &#8377; 94000</td>
+            </tr>
+
+            <tr>
+              <td>Making charges:</td>
+              <td> &#8377; 24000</td>
+            </tr>
+
+            <tr>
+              <td>Metal amount:</td>
+              <td>&#8377; 20000</td>
+            </tr>
+
+            <tr>
+              <td>Stone amount:</td>
+              <td>&#8377; 20000</td>
+            </tr>
+
+            <tr>
+              <td>GST:</td>
+              <td>1234567</td>
+            </tr>
+
+            <tr>
+              <td>Final product amount:</td>
+              <td>&#8377; 94000</td>
+            </tr>
+          </table>
+
+          <Button
+            className="btn btn-block bg-black btn-submit col-12 col-md-10 col-lg-6"
+            variant="contained"
+            onClick={handleOpenEnquiryModal}
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <span>ENQUIRY NOW</span>
+            <EastIcon />
+          </Button>
+
+          <EnquiryModal open={open} handleClose={handleCloseEnquiryModal} />
         </Grid>
       </Grid>
     </Box>

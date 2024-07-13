f6406fdf4cf42bc406e05df6695c200f05574fd5
Amit Solanki <amit.solanki@techneai.com>
2024-07-13 15:34:12 +0530

    zoom map onclick of location

--- src/Components/AllStores.js
+++ src/Components/AllStores.js
@@ -1,47 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import plusIcon from "../images/icons/plusicon.svg"
-import minusIcon from "../images/icons/minusicon.svg"
+import React, { useState, useEffect } from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import plusIcon from "../images/icons/plusicon.svg";
+import minusIcon from "../images/icons/minusicon.svg";
 
-export default function AllStores({ data }) {
-    const [expanded, setExpanded] = React.useState(false);
-    
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
+export default function AllStores({ data, handleAccordionClick }) {
+  const [expanded, setExpanded] = React.useState(false);
 
-    return (
-        <div className='faqs-container'>
-            {data.map((element, index) => (
-                <Accordion expanded={expanded === 'panel' + index} onChange={handleChange('panel' + index)} key={index}>
-                    <AccordionSummary
-                        expandIcon={expanded === 'panel' + index ? <img src={minusIcon} alt="rightArrowIcon" /> : <img src={plusIcon} alt="rightArrowIcon" />}
-                        aria-controls="panel1bh-content"
-                        id="panel1bh-header"
-                    >
-                        <Typography sx={{ width: '90%', flexShrink: 0 }}>
-                            {element.store_name}
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography className='find-store-description find-store-description-text'>
-                            {element.address}
-                        </Typography>
-                        <Typography className='find-store-description'>
-                            Toll Free No: {" "}
-                            <p className='toll-free-no'>
-                                {element.mobile}
-                            </p>
-                        </Typography>
-                        <Typography className='working-hours-container find-store-description'>
-                            <p>Working Hours: {" "}</p>
-                            <p className='workingHours-text-field'>{element.working_hrs}</p>
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>))}
-        </div>
-    );
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className="faqs-container">
+      {data.map((element, index) => (
+        <Accordion
+          expanded={expanded === "panel" + index}
+          onChange={handleChange("panel" + index)}
+          key={index}
+        >
+          <AccordionSummary
+            expandIcon={
+              expanded === "panel" + index ? (
+                <img src={minusIcon} alt="rightArrowIcon" />
+              ) : (
+                <img src={plusIcon} alt="rightArrowIcon" />
+              )
+            }
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+            onClick={() =>
+              handleAccordionClick({
+                lat: parseFloat(element.latitude),
+                lng: parseFloat(element.longitude),
+              })
+            }
+          >
+            <Typography sx={{ width: "90%", flexShrink: 0 }}>
+              {element.store_name}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography className="find-store-description find-store-description-text">
+              {element.address}
+            </Typography>
+            <Typography className="find-store-description">
+              Toll Free No: <p className="toll-free-no">{element.mobile}</p>
+            </Typography>
+            <Typography className="working-hours-container find-store-description">
+              <p>Working Hours: </p>
+              <p className="workingHours-text-field">{element.working_hrs}</p>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  );
 }
--- src/Components/Screens/FindAStore.js
+++ src/Components/Screens/FindAStore.js
@@ -1,152 +1,203 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import storeLocaterTitleImage from "../../images/storeLocaterTitleImageCropped.png";
-import { Button, Grid } from '@mui/material';
-import fillWhiteLeftIcon from '../../images/icons/fillWhiteLeftIcon.svg';
-import fillWhiteRightIcon from '../../images/icons/fillWhiteRightIcon.svg';
-import AllStores from '../AllStores';
-import { getStores, searchStores } from '../../services/FrontApp/index.service';
-import { GoogleMap, InfoWindow, Marker, useLoadScript } from "@react-google-maps/api";
+import { Button, Grid } from "@mui/material";
+import fillWhiteLeftIcon from "../../images/icons/fillWhiteLeftIcon.svg";
+import fillWhiteRightIcon from "../../images/icons/fillWhiteRightIcon.svg";
+import AllStores from "../AllStores";
+import { getStores, searchStores } from "../../services/FrontApp/index.service";
+import {
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useLoadScript,
+} from "@react-google-maps/api";
 
 export default function FindAStore() {
-    const [errors, setErrors] = useState([])
-    const center = {
-        lat: 18.5204,
-        lng: 73.8567
-      };
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_MAP_KEY // API key
-      });
-    const [markers, setMarkers] = useState([])
+  const [errors, setErrors] = useState([]);
+  const center = {
+    lat: 18.5204,
+    lng: 73.8567,
+  };
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_MAP_KEY, // API key
+  });
+  const [markers, setMarkers] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [map, setMap] = useState(null);
 
-    useEffect(() => {
-    }, [isLoaded])
-    const [searchData, setSearchData] = useState({
-        search: ''
-    })
+  useEffect(() => {}, [isLoaded]);
+  const [searchData, setSearchData] = useState({
+    search: "",
+  });
 
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const getData = async () => {
-        try {
-            const result = await getStores();
-            setData(result.data.data)
-            let markerData = []
-            for (let i = 0; i < result.data.data.length; i++) {
-                markerData[i] = {
-                    id: i + 1,
-                    address: result.data.data[i].address,
-                    position: { lat: parseFloat(result.data.data[i].latitude), lng: parseFloat(result.data.data[i].longitude)}
-                  };
-                
-            }
-            setMarkers(markerData)
-        } catch (error) {
-            console.error(error.message)
-        }
+  const getData = async () => {
+    try {
+      const result = await getStores();
+      setData(result.data.data);
+      let markerData = [];
+      for (let i = 0; i < result.data.data.length; i++) {
+        markerData[i] = {
+          id: i + 1,
+          address: result.data.data[i].address,
+          position: {
+            lat: parseFloat(result.data.data[i].latitude),
+            lng: parseFloat(result.data.data[i].longitude),
+          },
+        };
+      }
+      setMarkers(markerData);
+    } catch (error) {
+      console.error(error.message);
     }
-
-    useEffect(() => {
-        getData()
-    }, [])
-
-    const handleChange = ({ target }) => {
-        searchData[target.name] = target.value
-        const temp = Object.assign({}, searchData)
-        setSearchData(temp)
+  };
+  const handleAccordionClick = (position) => {
+    setSelectedLocation(position);
+    if (map) {
+      map.panTo(position);
+      map.setZoom(15);
     }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setErrors([])
-        // setSuccesMsg('')
-        try {
-            const result = await searchStores(searchData);
-            setData(result.data.data)
-            let markerData = []
-            for (let i = 0; i < result.data.data.length; i++) {
-                markerData[i] = {
-                    id: i + 1,
-                    address: result.data.data[i].address,
-                    position: { lat: parseFloat(result.data.data[i].latitude), lng: parseFloat(result.data.data[i].longitude)}
-                  };
-                
-            }
-            setMarkers(markerData)
-        } catch (error) {
-            setErrors(error.response.data.message)
-            setData([])
-        }
-    };
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const [activeMarker, setActiveMarker] = useState(null);
+  const handleChange = ({ target }) => {
+    searchData[target.name] = target.value;
+    const temp = Object.assign({}, searchData);
+    setSearchData(temp);
+  };
 
-    const handleActiveMarker = (marker) => {
-        if (marker === activeMarker) {
-            return;
-        }
-        setActiveMarker(marker);
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setErrors([]);
+    // setSuccesMsg('')
+    try {
+      const result = await searchStores(searchData);
+      setData(result.data.data);
+      let markerData = [];
+      for (let i = 0; i < result.data.data.length; i++) {
+        markerData[i] = {
+          id: i + 1,
+          address: result.data.data[i].address,
+          position: {
+            lat: parseFloat(result.data.data[i].latitude),
+            lng: parseFloat(result.data.data[i].longitude),
+          },
+        };
+      }
+      setMarkers(markerData);
+    } catch (error) {
+      setErrors(error.response.data.message);
+      setData([]);
+    }
+  };
 
-    const handleOnLoad = (map) => {
-        const bounds = new window.google.maps.LatLngBounds();
-        markers.forEach(({ position }) => bounds.extend(position));
-        map.fitBounds(bounds);
-    };
+  const [activeMarker, setActiveMarker] = useState(null);
 
-    return (
-        <div className='find-store-main-container'>
-            <div className='section-title' style={{ backgroundImage: `url(${storeLocaterTitleImage})` }}>
-                <div className="section-contents">
-                    <div className="header-title">
-                        <img src={fillWhiteLeftIcon} alt="React Logo" />
-                        <h4>Store Locator</h4>
-                        <img src={fillWhiteRightIcon} alt="React Logo" />
-                    </div>
-                    <div className="section-info">
-                        Come and say hello. Find your nearest store and check its opening hours and contact details.
-                    </div>
-                    <div className="find-store-change-location">
-                        <input type="text" placeholder='Enter Pincode or city' sx={{ border: 'none' }} name='search' value={searchData.search} onChange={handleChange} />
-                        <Button onClick={handleSubmit} className='location-change-button'>FIND STORES</Button>
-                    </div>
-                </div>
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
 
+  const handleOnLoad = (map) => {
+    console.log("map instance", map);
+    setMap(map);
+    const bounds = new window.google.maps.LatLngBounds();
+    markers.forEach(({ position }) => bounds.extend(position));
+    map.fitBounds(bounds);
+  };
 
-            </div>
-
-            <Grid container spacing={3} className='store-main-content'>
-                <Grid className='all-store-container' item xs={12} md={5}>
-                    <div className='all-store-titles p-3'>
-                        ALL STORES
-                    </div>
-                    <div className='store-locations-acordion'>
-                        <AllStores data={data} />
-                    </div>
-                </Grid>
-                <Grid className='find-store-google-map-container p-3' item xs={12} md={5}>
-                 {isLoaded && markers.length >= 0 ? (<GoogleMap
-                        onLoad={handleOnLoad}
-                        onClick={() => setActiveMarker(null)}
-                        mapContainerStyle={{ width: "100%", minHeight: "500px", height: "100%" }}
-                        center={center}
-                        zoom={10}
-                    >
-                        {markers.map(({ id, address, position }) => (
-                            <Marker
-                                key={id}
-                                position={position}
-                                onClick={() => handleActiveMarker(id)}
-                            >
-                                {activeMarker === id ? (
-                                    <InfoWindow onCloseClick={() => setActiveMarker(null)}>
-                                        <div><a href={`https://maps.google.com/?q=${position.lat},${position.lng}`} target='_blank'>{address}</a></div>
-                                    </InfoWindow>
-                                ) : null}
-                            </Marker>
-                        ))}
-                    </GoogleMap>) : null }
-                </Grid>
-            </Grid>
+  return (
+    <div className="find-store-main-container">
+      <div
+        className="section-title"
+        style={{ backgroundImage: `url(${storeLocaterTitleImage})` }}
+      >
+        <div className="section-contents">
+          <div className="header-title">
+            <img src={fillWhiteLeftIcon} alt="React Logo" />
+            <h4>Store Locator</h4>
+            <img src={fillWhiteRightIcon} alt="React Logo" />
+          </div>
+          <div className="section-info">
+            Come and say hello. Find your nearest store and check its opening
+            hours and contact details.
+          </div>
+          <div className="find-store-change-location">
+            <input
+              type="text"
+              placeholder="Enter Pincode or city"
+              sx={{ border: "none" }}
+              name="search"
+              value={searchData.search}
+              onChange={handleChange}
+            />
+            <Button onClick={handleSubmit} className="location-change-button">
+              FIND STORES
+            </Button>
+          </div>
         </div>
-    )
+      </div>
+
+      <Grid container spacing={3} className="store-main-content">
+        <Grid className="all-store-container" item xs={12} md={5}>
+          <div className="all-store-titles p-3">ALL STORES</div>
+          <div className="store-locations-acordion">
+            <AllStores
+              data={data}
+              handleAccordionClick={handleAccordionClick}
+            />
+          </div>
+        </Grid>
+        <Grid
+          className="find-store-google-map-container p-3"
+          item
+          xs={12}
+          md={5}
+        >
+          {isLoaded && markers.length >= 0 ? (
+            <GoogleMap
+              onLoad={handleOnLoad}
+              onClick={() => setActiveMarker(null)}
+              mapContainerStyle={{
+                width: "100%",
+                minHeight: "500px",
+                height: "100%",
+              }}
+              //   center={center}
+              //   zoom={10}
+              center={selectedLocation || center}
+              zoom={selectedLocation ? 15 : 10}
+            >
+              {markers.map(({ id, address, position }) => (
+                <Marker
+                  key={id}
+                  position={position}
+                  onClick={() => handleActiveMarker(id)}
+                >
+                  {activeMarker === id ? (
+                    <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                      <div>
+                        <a
+                          href={`https://maps.google.com/?q=${position.lat},${position.lng}`}
+                          target="_blank"
+                        >
+                          {address}
+                        </a>
+                      </div>
+                    </InfoWindow>
+                  ) : null}
+                </Marker>
+              ))}
+            </GoogleMap>
+          ) : null}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }

316a2fd169e3209c3cffddd934234880e945b14a
Somyaranjan <somyaranjan.sethy@techneai.com>
2024-07-13 15:41:49 +0530

    config webpack

--- src/App.js
+++ src/App.js
@@ -1,90 +1,56 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './scss/style.scss';
-import HomeLayout from './HomeLayout';
-import DashboardLayout from './DashboardLayout';
-import OrderLayout from './OrderLayout';
-import { frontAppRoutes, dashboardRoutes, orderRoutes, normalRouts } from "./routes";
-import NormalLayout from './NormalLayout';
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { useScrollToTop } from "./hooks";
+import {
+  frontAppRoutes,
+  dashboardRoutes,
+  orderRoutes,
+  normalRouts,
+} from "./routes";
+import HomeLayout from "./HomeLayout";
+import DashboardLayout from "./DashboardLayout";
+import OrderLayout from "./OrderLayout";
+import NormalLayout from "./NormalLayout";
+
+import "./scss/style.scss";
+
+const renderRoutes = (routes) =>
+  routes.map((route, indx) =>
+    route.element ? (
+      <Route
+        key={indx}
+        path={route.path}
+        element={route.element}
+        exact={route.exact}
+        strict={route.strict}
+      />
+    ) : null
+  );
 
 function App() {
+  useScrollToTop();
+
   return (
-    <>
-      <div className="main-container">
-        <BrowserRouter>
-          <div className="main-body-container">
-            <Suspense>
-              <Routes>
-                <Route
-                  path='/success'
-                  element={<NormalLayout />}
-                >
-                  {normalRouts.map(
-                    (route, indx) => route.element && (
-                      <Route
-                        key={indx}
-                        path={route.path}
-                        element={route.element}
-                        exact={route.exact}
-                        strict={route.strict}
-                      />
-                    ),
-                  )}
-                </Route>
-                <Route
-                  path='/dashboard'
-                  element={<DashboardLayout />}
-                >
-                  {dashboardRoutes.map(
-                    (route, indx) => route.element && (
-                      <Route
-                        key={indx}
-                        path={route.path}
-                        element={route.element}
-                        exact={route.exact}
-                        strict={route.strict}
-                      />
-                    ),
-                  )}
-                </Route>
-                <Route
-                  path='/order'
-                  element={<OrderLayout />}
-                >
-                  {orderRoutes.map(
-                    (route, indx) => route.element && (
-                      <Route
-                        key={indx}
-                        path={route.path}
-                        element={route.element}
-                        exact={route.exact}
-                        strict={route.strict}
-                      />
-                    ),
-                  )}
-                </Route>
-                <Route
-                  path='/'
-                  element={<HomeLayout />}
-                >
-                  {frontAppRoutes.map(
-                    (route, indx) => route.element && (
-                      <Route
-                        key={indx}
-                        path={route.path}
-                        element={route.element}
-                        exact={route.exact}
-                        strict={route.strict}
-                      />
-                    ),
-                  )}
-                </Route>
-              </Routes>
-            </Suspense>
-          </div>
-        </BrowserRouter>
+    <div className="main-container">
+      <div className="main-body-container">
+        <Suspense>
+          <Routes>
+            <Route path="/success" element={<NormalLayout />}>
+              {renderRoutes(normalRouts)}
+            </Route>
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              {renderRoutes(dashboardRoutes)}
+            </Route>
+            <Route path="/order" element={<OrderLayout />}>
+              {renderRoutes(orderRoutes)}
+            </Route>
+            <Route path="/" element={<HomeLayout />}>
+              {renderRoutes(frontAppRoutes)}
+            </Route>
+          </Routes>
+        </Suspense>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+    publicPath: "/", // Adjust as per your deployment setup
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[hash].[ext]",
+              publicPath: "../",
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    runtimeChunk: "single",
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            pure_funcs: ["console.log"],
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new Dotenv(), // Load environment variables from .env file
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
+  devtool: "source-map",
+};

d3f794d59bbc01e102ff2e915b5159c0bd52dba7
AsmitaMargaje <asmita.margaje@techneai.com>
2024-10-24 12:27:15 +0530

    resolved issue

--- src/Components/Screens/Product-catalogue/ProductCatalogues.js
+++ src/Components/Screens/Product-catalogue/ProductCatalogues.js
@@ -332,7 +332,7 @@ export const ProductCatalogues = () => {
 
   return (
     <div className="product-catalogues">
-      <div className="product-catalogue-banner " style={{ height: "65vh" }}>
+      <div className="product-catalogue-banner  ">
         <img src={banner} alt="Banner image" />
       </div>
       <Paper

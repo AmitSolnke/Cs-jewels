3ffeee3131bcca13050c2854a39fdb03042ae3ec
AsmitaMargaje <asmita.margaje@techneai.com>
2025-02-10 18:32:22 +0530

    working on ui chnages

--- src/HomeLayout.js
+++ src/HomeLayout.js
@@ -1,23 +1,27 @@
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import { Outlet } from 'react-router-dom';
-import { ShoppingBag } from './Components/Screens/ShoppingBag';
-import { useState } from 'react';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Outlet } from "react-router-dom";
+import { ShoppingBag } from "./Components/Screens/ShoppingBag";
+import { useState } from "react";
 
 const HomeLayout = () => {
-    const [openDrawer, setOpenDrawer] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
 
-    const handleOpenDrawer = () => {
-        setOpenDrawer(!openDrawer);
-    };
-    return (
-        <div>
-            <ShoppingBag open={openDrawer} handleDrawer={handleOpenDrawer} style={{display: 'none'}} />
-            <Header openDrawer={openDrawer} handleOpenDrawer={handleOpenDrawer} />
-            <Outlet className="outlet-container" />
-            <Footer />
-        </div>
-    )
-}
+  const handleOpenDrawer = () => {
+    setOpenDrawer(!openDrawer);
+  };
+  return (
+    <div>
+      <ShoppingBag
+        open={openDrawer}
+        handleDrawer={handleOpenDrawer}
+        style={{ display: "none" }}
+      />
+      <Header openDrawer={openDrawer} handleOpenDrawer={handleOpenDrawer} />
+      <Outlet className="outlet-container" />
+      <Footer />
+    </div>
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;

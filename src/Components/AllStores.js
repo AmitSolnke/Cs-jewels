0d1f37b0b2062c1d6cf6635b455411410c478f6d
Amit Solanki <amit.solanki@techneai.com>
2025-02-17 13:12:58 +0530

    Google direction api integration for map

--- src/Components/AllStores.js
+++ src/Components/AllStores.js
@@ -35,6 +35,7 @@ export default function AllStores({ data, handleAccordionClick }) {
               handleAccordionClick({
                 lat: parseFloat(element.latitude),
                 lng: parseFloat(element.longitude),
+                element
               })
             }
           >

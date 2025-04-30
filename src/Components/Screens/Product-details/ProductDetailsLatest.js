fec35a5fbccaa9c49a6f2c454edfa4918a6ffed1
Rohit Ghadage <rohit.ghadage@techneai.com>
2025-04-30 17:32:35 +0530

    product-detail-page

--- src/Components/Screens/Product-details/ProductDetailsLatest.js
+++ src/Components/Screens/Product-details/ProductDetailsLatest.js
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
-import { Box, Button, Grid } from "@mui/material";
-import EastIcon from "@mui/icons-material/East";
-import EnquiryModal from "./EnquiryModal";
-import { getProductDetails } from "../../../services/FrontApp/index.service";
-import SliderImage from "react-zoom-slider";
-import { SideBySideMagnifier } from "react-image-magnifiers";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import { Box, Button, Grid, Stack } from '@mui/material';
+import EastIcon from '@mui/icons-material/East';
+import EnquiryModal from './EnquiryModal';
+import { getProductDetails } from '../../../services/FrontApp/index.service';
+import SliderImage from 'react-zoom-slider';
+import { SideBySideMagnifier } from 'react-image-magnifiers';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import ForwardIcon from '@mui/icons-material/Forward';
+import 'slick-carousel/slick/slick-theme.css';
+import ProductDetailsTabs from './ProductDetailsTabs';
 
 export const ProductDetailsLatest = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [productDetails, setProductDetails] = useState({
-    product_name: "",
-    metal_description: "",
-    metal_amount: "",
-    item_description: "",
-    purity: "",
-    description: "",
+    product_name: '',
+    metal_description: '',
+    metal_amount: '',
+    item_description: '',
+    purity: '',
+    description: '',
     sales_price: 0,
     gross_wt: 0,
     stones_details: [],
-    images: [],
+    images: []
   });
 
   const getProductData = async () => {
@@ -70,16 +72,16 @@ export const ProductDetailsLatest = () => {
   // Todo: this is the image array refactor code to use images from the api call
   const imageItems = productDetails?.images?.map((image) => ({
     original: image.image_path,
-    thumbnail: image.image_path,
+    thumbnail: image.image_path
   }));
   const sliderData =
     imageItems && imageItems.length > 0
       ? imageItems
-        .filter((item) => item && item.original) // Filter out invalid items
-        .map((item) => ({
-          image: item.original, // Use `original` field for the `image`
-          description: "", // Add descriptions if needed, or leave empty
-        }))
+          .filter((item) => item && item.original) // Filter out invalid items
+          .map((item) => ({
+            image: item.original, // Use `original` field for the `image`
+            description: '' // Add descriptions if needed, or leave empty
+          }))
       : [];
 
   const [open, setOpen] = useState(false);
@@ -100,10 +102,10 @@ export const ProductDetailsLatest = () => {
             src={imageItems?.[i]?.original}
             alt={`Thumbnail ${i + 1}`}
             style={{
-              width: "60px",
-              height: "45px",
+              width: '60px',
+              height: '45px',
               // objectFit: "cover",
-              borderRadius: "5px",
+              borderRadius: '5px'
             }}
           />
         </a>
@@ -115,12 +117,12 @@ export const ProductDetailsLatest = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
-    dotsClass: "slick-dots slick-thumb",
+    dotsClass: 'slick-dots slick-thumb',
     autoplay: false,
     waitForAnimate: false,
     fade: true,
     adaptiveHeight: true,
-    infinite: true,
+    infinite: true
     // responsive: [
     //   {
     //     breakpoint: 768,
@@ -132,13 +134,65 @@ export const ProductDetailsLatest = () => {
     // ],
   };
 
+  const [active, setActive] = useState('general details');
+  const [content, setContent] = useState('');
+
+  const handleProductDetailsTab = (product) => {
+    if (active.toLowerCase() === 'metal details') {
+      setContent(
+        <>
+          <Box>
+            <span className="fw-bolder"> Gross weight</span>:{product?.gross_wt}
+            g
+          </Box>
+          <Box>
+            <span className="fw-bolder"> Net weight</span> :{product?.net_wt}g
+          </Box>
+          <Box>
+            <span className="fw-bolder"> Purity</span> : {product?.purity}K
+          </Box>
+        </>
+      );
+    } else if (active.toLowerCase() === 'product description') {
+      setContent(
+        <>
+          {product?.description ||
+            'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}
+        </>
+      );
+    } else {
+      setContent(
+        <>
+          <Box>
+            <span className="fw-bolder"> Product Product Name</span> :{' '}
+            {product?.product_name || '--'}
+          </Box>
+          <Box>
+            <span className="fw-bolder"> Product Code </span> :{' '}
+            {product?.product_code || '--'}
+          </Box>
+          <Box>
+            <span className="fw-bolder"> Gender </span> :{' '}
+            {product?.gender || '--'}
+          </Box>
+        </>
+      );
+    }
+  };
+  const handleActiveTab = (tab) => {
+    setActive(tab);
+  };
+
+  useEffect(() => {
+    handleProductDetailsTab(productDetails);
+  }, [active, JSON.stringify(productDetails)]);
   return (
     <Box className="product-details-latest">
       <Grid
         container
         spacing={1}
         className="p-3 grid-container"
-        style={{ background: "#fff" }}
+        style={{ background: '#fff' }}
       >
         <Grid item md={6}>
           <div className="">
@@ -165,8 +219,8 @@ export const ProductDetailsLatest = () => {
                         // overlayBackgroundColor="rgba(0,0,0,0.3)"
                         // className="custom-magnifier"
                         fillAvailableSpace={false}
-                      // transitionSpeed={0.2}
-                      // overlayBackgroundColor="rgba(0,0,0,0.6)"
+                        // transitionSpeed={0.2}
+                        // overlayBackgroundColor="rgba(0,0,0,0.6)"
                       />
                     </div>
                   ))}
@@ -190,8 +244,9 @@ export const ProductDetailsLatest = () => {
         <Grid
           item
           md={6}
-          style={{ paddingLeft: "1rem" }}
-          className="product-details-wrapper"
+          style={{ paddingLeft: '1rem' }}
+          className="product-details-wrapper pt-md-0"
+          width={'100%'}
         >
           {/* <span className="new-arrival-badge">NEW ARRIVAL</span>
 
@@ -208,16 +263,25 @@ export const ProductDetailsLatest = () => {
               {productDetails.item_description}
             </div> */}
 
-          <div className=" m-2 product-details">
-            <div className="product-details-title-link">PRODUCT DETAILS</div>
-            <div>Gross weight: {productDetails.gross_wt}g</div>
-            <div>Net weight: {productDetails.net_wt}g</div>
-            <div>Purity: {productDetails.purity}</div>
-            {productDetails.stones_details.map((stone, index) => (
-              <div>
-                Stone {index + 1} weight: {stone.net_wt}g
-              </div>
-            ))}
+          <div className=" m-2 product-details w-100 pt-md-0">
+            <div
+              className="product-details-title-link"
+              style={{
+                font:'normal normal 900 17px/19px "Afacad Flux", serif'
+              }}
+            >
+              PRODUCT DETAILS
+            </div>
+            <div className="product-description my-3">
+              <span style={{ color: '#666666', fontWeight: '600' }}>
+                Regular Price(Starting From)
+              </span>
+              :
+              <span style={{ color: '#000', fontWeight: '600' }}>
+                {productDetails.regular_price}/-
+              </span>
+            </div>
+            <ProductDetailsTabs productDetails={productDetails} />
           </div>
 
           {/* commented temporary
@@ -252,22 +316,27 @@ export const ProductDetailsLatest = () => {
               <td>&#8377; {productDetails.sales_price}</td>
             </tr>
           </table> */}
+          <Box width={{ md: 'auto',sm:'50%', xs: '60%' }}>
+            <Button
+              className="btn btn-block bg-black btn-submit col-12 col-md-10 col-lg-6 mx-2"
+              variant="contained"
+              onClick={handleOpenEnquiryModal}
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: '3rem'
+              }}
+            >
+              <span className="button-enquire">ENQUIRE</span>
+              <EastIcon />
+            </Button>
 
-          <Button
-            className="btn btn-block bg-black btn-submit col-12 col-md-10 col-lg-6 mx-2"
-            variant="contained"
-            onClick={handleOpenEnquiryModal}
-            style={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <span className="button-enquire">ENQUIRE</span>
-            <EastIcon />
-          </Button>
-
-          <EnquiryModal
-            open={open}
-            handleClose={handleCloseEnquiryModal}
-            productId={id}
-          />
+            <EnquiryModal
+              open={open}
+              handleClose={handleCloseEnquiryModal}
+              productId={id}
+            />
+          </Box>
         </Grid>
       </Grid>
     </Box>

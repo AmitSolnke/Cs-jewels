b432c244ece61cee7cb0e3c52c1241ef86472b87
Rohit Ghadage <rohit.ghadage@techneai.com>
2025-05-13 18:50:52 +0530

    toggle-mapstore-fix

--- src/Components/Screens/FindAStore.js
+++ src/Components/Screens/FindAStore.js
@@ -66,7 +66,7 @@ export default function FindAStore() {
         map.setZoom(20);
       }
     } else {
-      setToggleMap(false);
+      setToggleMap((prevToggleMap) => !prevToggleMap);
     }
   };
 

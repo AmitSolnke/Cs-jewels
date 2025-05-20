ea416492ffc29c43430f874d65b862d78f352dca
Rohit Ghadage <rohit.ghadage@techneai.com>
2025-05-20 16:32:18 +0530

    added-scheme-navigation

--- src/Components/Screens/Scheme/GoldenDreamPlanScheme.js
+++ src/Components/Screens/Scheme/GoldenDreamPlanScheme.js
@@ -3,12 +3,16 @@ import {
   EarnBigger,
   GoldenDreamLogo,
   GoldenDreams,
+  InvestLittle,
   RateImage,
   SchemeApplicable
 } from '../../../images/SchemeImages';
 import { handleDownload } from '../../../utilities/handleDownload';
-import HorizonatalBanner from '../../Common/HorizonatalBanner';
+import { useNavigate } from 'react-router-dom';
+import { GDPButton } from './style';
 const GoldenDreamPlanScheme = () => {
+
+  const navigate = useNavigate();
   return (
     <Container maxWidth="lg" sx={{ my: 2 }}>
       <Stack gap={2} sx={{ width: '100%', height: '100%' }}>
@@ -69,12 +73,18 @@ const GoldenDreamPlanScheme = () => {
             />
           </Stack>
         </Box>
-        <Box sx={{ width: '100%', height: '100%' }}>
-          <HorizonatalBanner
-            imageSrc={GoldenDreamLogo}
-            title={'Invest a Little, Achieve a Lot - GDP'}
-            desc={'To Enroll Now Connect our Representative On 07969991827'}
+        <Box sx={{ width: '100%', height: '100%', position: 'relative' }}>
+          <img
+            src={InvestLittle}
+            alt="InvestLittle"
+            style={{ width: '100%', height: '100%' }}
           />
+          <GDPButton
+            variant="contained"
+            onClick={() => navigate('/')}
+          >
+            Enroll Now
+          </GDPButton>
         </Box>
       </Stack>
     </Container>

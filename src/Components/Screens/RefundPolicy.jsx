f7a9adcd632ba3e032aa8d3ac5eb0ec4f398cdcb
Rohit Ghadage <rohit.ghadage@techneai.com>
2025-05-21 13:51:53 +0530

    changed-policies

--- src/Components/Screens/RefundPolicy.jsx
+++ src/Components/Screens/RefundPolicy.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import { Box, Divider, Grid } from "@mui/material";
 
 function RefundPolicy() {
   return (
@@ -24,7 +24,9 @@ function RefundPolicy() {
             <div className="empty-container-section">xs=6 md=4</div>
           </Grid>
           <Grid item xs={10} md={4}>
-            <div className="privacy-policy-header-text">ONLINE RETURN POLICY</div>
+            <div className="privacy-policy-header-text">
+              ONLINE RETURN POLICY
+            </div>
             <div className="privacy-policy-normal-text">
               We offer an easy replacement option within 10 days of the delivery
               date.
@@ -44,7 +46,7 @@ function RefundPolicy() {
           </Grid>
           <Grid item xs={10} md={4}>
             <div className="privacy-policy-header-text">
-            ONLINE CANCELLATION POLICY
+              ONLINE CANCELLATION POLICY
             </div>
             <div className="privacy-policy-normal-text">
               <ol className="ps-3">
@@ -64,6 +66,36 @@ function RefundPolicy() {
             </div>
             {/* <Divider className='divider-privacy-policy' /> */}
             <hr />
+          </Grid>
+          <Grid item xs={1} md={4}>
+            <div className="empty-container-section">xs=6 md=4</div>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box className="privacy-policy-container" sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={1} md={4}>
+            <div className="empty-container-section">xs=6 md=4</div>
+          </Grid>
+          <Grid item xs={10} md={4}>
+            <div className="privacy-policy-header-text text-uppercase">
+              Scheme Cancellation Policy
+            </div>
+            <div className="privacy-policy-normal-text">
+              <ol className="ps-3">
+                <li>
+                  Customer will get full refund of the principal amount
+                  deposited in the scheme by visiting nearest branch and after
+                  submission of original Passbook with valid Id proof.
+                </li>
+
+                <li className="privacy-policy-normal-text">
+                  The amount will be paid only through online transfer to
+                  customer’s personal bank account only.
+                </li>
+              </ol>
+            </div>
+            <Divider className="divider-privacy-policy" />
           </Grid>
           <Grid item xs={1} md={4}>
             <div className="empty-container-section">xs=6 md=4</div>

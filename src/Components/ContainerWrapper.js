d09ae4db7a4fa52517c4babd5d8b0eda0dbe430d
intern <intern@techneai.com>
2025-06-02 14:57:00 +0530

    Add MobileMenu component and integrate it into Header; create styled DropdownWrapper for mobile hamburger menu view.

---
+++ src/Components/ContainerWrapper.js
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+const ContainerWrapper = ({ children, sx = {} }) => {
+    return (
+        <Container maxWidth="lg" sx={sx}>
+            {children}
+        </Container>
+    );
+};
+
+export default ContainerWrapper;
